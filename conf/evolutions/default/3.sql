baaa3a0d81ed4384d27beb7334347080ed016bac
Giovanni Gargiulo <ggargiulo@gilt.com>
2017-01-25 14:11:51 +0000

    added the default value to notifications

--- conf/evolutions/default/3.sql
+++ conf/evolutions/default/3.sql
@@ -1,5 +1,5 @@
 # --- !Ups
-ALTER TABLE process_definition ADD COLUMN notifications JSONB NOT NULL;
+ALTER TABLE process_definition ADD COLUMN notifications JSONB NOT NULL DEFAULT '[{}]'::jsonb;
 
 # --- !Downs
 ALTER TABLE process_definition DROP COLUMN IF EXISTS notifications;

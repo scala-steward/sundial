2d666aa44ed617455e27b93af3f07ec212df80da
Giovanni Gargiulo <ggargiulo@gilt.com>
2017-12-05 11:39:20 +0000

    Deleted unused dockerfile, added require to ENV vars

--- deploy.sh
+++ deploy.sh
@@ -1,11 +1,14 @@
 #!/bin/sh
 
+: "${BUILD_ID?Need to set BUILD_ID}"
+: "${DOCKER_REGISTRY?Need to set DOCKER_REGISTRY}"
+: "${ECS_COMPANION_TAG?Need to set ECS_COMPANION_TAG}"
+# An s3 bucket for deployment assets
+: "${CODEDEPLOY_S3_BUCKET?Need to set CODEDEPLOY_S3_BUCKET}"
+
 #Change these to match your environment
 REGION=us-east-1
-# DOCKER_REGISTRY=
 DOCKER_REPO=sundial
-# An s3 bucket for deployment assets
-# CODEDEPLOY_S3_BUCKET=
 
 sbt docker:publishLocal
 
@@ -17,6 +20,12 @@ BUILD_ID=$(git describe --tags --dirty --always | sed "s/^v//")
 docker tag $DOCKER_REPO:$BUILD_ID $DOCKER_REGISTRY/$DOCKER_REPO:$BUILD_ID
 
 docker push $DOCKER_REGISTRY/$DOCKER_REPO:$BUILD_ID
+
+echo "BUILD_ID: $BUILD_ID"
+echo "DOCKER_REPO: $DOCKER_REPO"
+echo "DOCKER_REGISTRY: $DOCKER_REGISTRY"
+echo "ECS_COMPANION_TAG:  $ECS_COMPANION_TAG"
+echo "CODEDEPLOY_S3_BUCKET:  $CODEDEPLOY_S3_BUCKET"
 
 m4 -D_BUILD_ID_=$BUILD_ID -D_DOCKER_REPO_=$DOCKER_REPO -D_DOCKER_REGISTRY_=$DOCKER_REGISTRY -D_ECS_COMPANION_TAG_=$ECS_COMPANION_TAG codedeploy/start.template > codedeploy/start.sh
 m4 -D_BUILD_ID_=$BUILD_ID codedeploy/healthcheck.template > codedeploy/healthcheck.sh
